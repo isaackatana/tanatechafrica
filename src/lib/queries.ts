ccf71c55112c02051af2c39c196550b4be0c59fd
isaackatana <mrisaackatana@gmail.com>
2025-07-23 23:30:17 +0300

    ==

--- src/lib/queries.ts
+++
@@ -1,9 +0,0 @@
-import groq from 'groq';
-
-export const postBySlugQuery = groq`
-  *[_type == "post" && slug.current == $slug][0] {
-    title,
-    publishedAt,
-    body
-  }
-`;
--- src/lib/sanityClient.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from 'next-sanity';
-
-export const sanity = createClient({
-  projectId: 'yourProjectId', // Replace with your actual
-  dataset: 'production',
-  useCdn: false,
-  apiVersion: '2023-07-23',
-});
